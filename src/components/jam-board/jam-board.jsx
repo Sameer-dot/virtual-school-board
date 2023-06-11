ebf42c5ed8ea7f860ae56425338674727a0f1dd9
Sameer-dot <sameernaveed37@gmail.com>
2023-06-12 01:25:52 +0500

    jamboard changes

--- src/components/jam-board/jam-board.jsx
+++ src/components/jam-board/jam-board.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React,{useEffect,useRef} from "react";
 import login from "../../assets/images/login.svg";
 import Header from "./../header/header";
 import Footer from "./../footer/footer";
 
 const JamBoard = () => {
+
+
+   const imgRef = useRef();
+
+   useEffect(() => {
+     imgRef.current.src =
+       "https://dfc2-119-155-13-241.ngrok-free.app/api/cv/video_feed";
+   }, []);
+
   return (
     <>
       <Header />
       <div style={{ height: "100vh", background: "#000" }}>
         <div className="d-flex flex-column justify-content-center align-items-center">
-          <img src={login} alt="under development" />
-          <h1 className="mt-4" style={{ color: "#fff" }}>
-            This feature is under Construction🏗
-          </h1>
+          <img style={{height: "625px", width: "1200px"}} ref={imgRef} alt="Video Feed" />
         </div>
       </div>
       <Footer />
